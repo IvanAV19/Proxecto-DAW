4ddd92c10e1ed2d30a58de12b104713a7c5e87ab
IvanAV19 <82937686+IvanAV19@users.noreply.github.com>
2024-05-16 13:26:22 +0200

    Servicios

---
+++ devicecare/app/Services/Technicians.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace
+ App\Services;
+
+ use App\Models\User;
+
+ class Technicians
+ {
+     public function get()
+     {
+         $technicians = User::where('id', '!=', 1)->get(); // 1 = admin
+         $data = [];
+
+         foreach($technicians as $technician){
+             $data[$technician->id] = $technician->name;
+         }
+
+         return $data;
+     }
+ }
